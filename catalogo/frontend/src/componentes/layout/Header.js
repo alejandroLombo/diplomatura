f87c6a7c0b68a29342036e13a1cf32321c404029
alejandroLombo <lombo.alejandro@gmail.com>
2023-06-03 19:52:37 -0300

    avances

---
+++ catalogo/frontend/src/componentes/layout/Header.js
@@ -0,0 +1,10 @@
+const Header = (props) => {
+    return (
+        <div className="holder">
+            <img src="img/logo.png" width="100" alt=""></img>
+                <h1>Transporte 2023</h1>
+        </div>
+    )
+}
+
+export default Header;
