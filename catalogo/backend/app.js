9bc7c321730b3e629a5be5c8f77aacadb9b9e274
alejandroLombo <lombo.alejandro@gmail.com>
2023-07-03 14:52:02 -0300

    m4u4

--- catalogo/backend/app.js
+++ catalogo/backend/app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 var productosRouter = require('./routes/productos');
@@ -21,6 +23,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret:'klashjdfgqwur',
+  resave:false,
+  saveUninitialized:true,
+}));
+
 
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
